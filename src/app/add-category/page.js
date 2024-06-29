5c69aed0015b2deaa1399e061c9c130fc3a8108e
Philip Henning <philiphenning@outlook.com>
2024-06-30 00:02:57 +0100

    can now add categories

---
+++ src/app/add-category/page.js
@@ -0,0 +1,90 @@
+import { connect } from "@/utilities/connect"
+import { revalidatePath } from "next/cache"
+
+export default async function AddCategory(){
+
+    const db = connect()    
+
+    const categories = (await db.query(`SELECT DISTINCT *
+        FROM categories
+        ORDER BY category_name asc`)).rows 
+
+    async function handleAddCategory(formData){
+        'use server'
+
+        const category = formData.get('category')
+
+        const db = connect()
+
+        db.query(`INSERT INTO categories (category_name)
+                VALUES ($1)`, [category])
+      
+    
+        revalidatePath(`/add-category`)
+         
+    }
+        
+    return(
+        <div>
+
+            {categories.map( (category) => {
+               return(
+                   <h1>{category.category_name}</h1>
+               )
+           })}
+        
+            <form action={handleAddCategory} className="flex flex-col">
+
+                <input id="category" name="category" class="" type="text" placeholder="Create a new category"></input>
+
+                <button type="submit">Submit</button> 
+
+            </form>
+
+        </div>
+    )
+}
+
+// import { connect } from "@/utilities/connect"
+// import { revalidatePath } from "next/cache"
+
+// export default async function AddCategory(){
+
+//     const db = connect()
+
+//     const categories = (await db.query(`SELECT DISTINCT *
+//                                         FROM categories
+//                                         ORDER BY category_name asc`)).rows
+
+//     async function handleAddCategory(formData){
+//         'use server'
+
+//         const category = formData.get('category')
+
+//         console.log(formData)
+
+//         // db.query(`INSERT INTO categories (category_name) VALUES ($1)`, [category])
+
+//         // revalidatePath(`/add-category`)
+//     }
+
+//     return(
+//         <div>
+
+//             {categories.map( (category) => {
+//                 return(
+//                     <h1>{category.category_name}</h1>
+//                 )
+//             })}
+        
+//             <form onSubmit={handleAddCategory} className="flex flex-col">
+
+//                 <input id="category" name="category" class="" type="text" placeholder="Add a new category"></input>
+
+//                 <button type="submit">Submit</button> 
+
+//             </form>
+
+//         </div>
+//     )
+// }
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,7 +5,9 @@ export function Footer(){
         <div className="flex justify-center p-3">
             
             <div className="footer">
-                footer
+                <Link href="/add-category">Add Category</Link>
+                &nbsp;|&nbsp;
+                <Link href="/add-show">Add Show</Link>
             </div>
                   
         </div>
